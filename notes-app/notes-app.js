9eb387407d0bd684e120fe7fdf2ee162278472ab
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-24 18:29:58 +0300

    Setup UUID Library

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,42 +1,21 @@
-const notes = [
-	{
-		title: 'Learn Arabic',
-		body: 'I will take 3 years for you to master it',
-	},
-	{
-		title: 'Learn Persian',
-		body: 'I will take 2 years for you to master it',
-	},
-	{
-		title: 'Speak fluent English',
-		body: 'I will take 1 years for you to master it',
-	},
-];
+const notes = getSavedNotes();
 
 const filters = {
 	searchText: '',
 };
 
-const renderNotes = function (notes, filters) {
-	const filteredNotes = notes.filter(function (note) {
-		return note.title
-			.toLowerCase()
-			.includes(filters.searchText.toLowerCase());
-	});
-
-	document.querySelector('#notes').innerHTML = '';
-
-	filteredNotes.forEach(function (note) {
-		const noteEl = document.createElement('p');
-		noteEl.textContent = note.title;
-		document.querySelector('#notes').appendChild(noteEl);
-	});
-};
-
 renderNotes(notes, filters);
 
+// Event Handlers
 document.querySelector('#add-notes').addEventListener('click', function (e) {
-	e.target.textContent = 'Button was clicked..';
+	notes.push({
+		id: uuidv4(),
+		title: '',
+		body: '',
+	});
+
+	saveNotes(notes);
+	renderNotes(notes, filters);
 });
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
@@ -44,8 +23,6 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 	renderNotes(notes, filters);
 });
 
-document.querySelector('#name-form').addEventListener('submit', function (e) {
-	e.preventDefault();
-	console.log(e.target.elements.firstName.value);
-	e.target.elements.firstName.value = '';
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+	console.log(e.target.value);
 });
